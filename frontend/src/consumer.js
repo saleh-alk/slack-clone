8590e072fdad971a07630c0dac5e78e327f2eb93
Saleh Alkaheli <skalkaheli@gmail.com>
2023-01-12 17:14:41 -0800

    fixing redis

--- frontend/src/consumer.js
+++ frontend/src/consumer.js
@@ -4,6 +4,6 @@ let wsUrl;
 if (process.env.NODE_ENV !== "production")
     wsUrl = "ws://localhost:5000/cable";
 else
-    wsUrl = "/cable";
+    wsUrl = "https://khakis.onrender.com/cable";
 
 export default createConsumer(wsUrl);
